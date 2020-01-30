1aeeff4fa052e65e4b88e628f038528dffc226ae
Kelby Mittan <kelbymittan@pursuit.org>
2020-01-30 18:03:08 -0500

    added did sets

--- WeatherApp/WeatherApp/ViewController.swift
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lat = 0.0 {
+        didSet {
+            print(lat)
+        }
+    }
+    
+    var long = 0.0 {
+        didSet {
+            print(long)
+        }
+    }
+    
+    var zipCodeString = "" {
+        didSet {
+            print(zipCodeString)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        getZip(search: "10019")
+        //        print(lat)
+        //        print(long)
+//        print("\(lat.description),\(long.description)")
     }
-
-
+    
+    
+    
+    
+    func getZip(search: String) {
+        ZipCodeHelper.getLatLong(fromZipCode: search) { (result) in
+            switch result {
+            case .failure(let appError):
+                print("\(appError)")
+                
+            case .success(let latLong):
+                self.lat = latLong.lat
+                self.long = latLong.long
+                self.zipCodeString = "\(latLong.lat),\(latLong.long)"
+//                print("\(self.lat.description),\(self.long.description)")
+            }
+        }
+    }
+    
 }
 

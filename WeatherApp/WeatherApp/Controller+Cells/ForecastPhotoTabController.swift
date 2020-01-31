639457a64b47692cc8429ad342962d1d03f2d57c
Kelby Mittan <kelbymittan@pursuit.org>
2020-01-31 16:19:43 -0500

    new files for forecast view and tab bar controller

---
+++ WeatherApp/WeatherApp/Controller+Cells/ForecastPhotoTabController.swift
@@ -0,0 +1,21 @@
+//
+//  ForecastPhotoTabController.swift
+//  WeatherApp
+//
+//  Created by Kelby Mittan on 1/31/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class ForecastPhotoTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ WeatherApp/WeatherApp/Controller+Cells/ForecastView.swift
@@ -0,0 +1,15 @@
+//
+//  ForecastView.swift
+//  WeatherApp
+//
+//  Created by Kelby Mittan on 1/31/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class ForecastView: UIView {
+
+    
+
+}

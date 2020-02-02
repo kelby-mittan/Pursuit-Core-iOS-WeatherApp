452dd761f6af6e392bd6c7bfb1a64ed7344f08f4
Kelby Mittan <kelbymittan@pursuit.org>
2020-02-02 15:08:49 -0500

    setting up files for fav pixabay collection view

---
+++ WeatherApp/WeatherApp/Cells/FavPixCell.swift
@@ -0,0 +1,13 @@
+//
+//  FavPixCell.swift
+//  WeatherApp
+//
+//  Created by Kelby Mittan on 2/2/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class FavPixCell: UICollectionViewCell {
+    
+}
---
+++ WeatherApp/WeatherApp/PixabayControllers+Views/PixabayCollectionView.swift
@@ -0,0 +1,50 @@
+//
+//  PixabayCollectionView.swift
+//  WeatherApp
+//
+//  Created by Kelby Mittan on 2/2/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class PixabayCollectionView: UIView {
+    
+    public lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: 120, height: 200)
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        cv.backgroundColor = .systemBackground
+        return cv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setupCollectionViewConstraints()
+    }
+    
+    private func setupCollectionViewConstraints() {
+        addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+
